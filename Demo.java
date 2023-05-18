7a0c4f104f70e560a1098f9a2fcbeec1d2fd7a08
Avinashgbadiger <133941890+Avinashgbadiger@users.noreply.github.com>
2023-05-18 10:37:19 +0530

    Update_Java_Program

--- Demo.java
+++ Demo.java
@@ -2,6 +2,16 @@ class Demo
 {
  public static void main(String [] args)
  {
+  Demo2 demo2=new Demo2();
+  demo2.disp();
   System.out.println(" Hello Git Hub this is my First File"); 
  }
+}
+class Demo2
+{
+ public void disp()
+ {
+   System.out.println(" This is a change ");
+ }
+ 
 }
